4548282f5bb5118d9543f7b49fdb02fa4209ee17
ElaheShadman <elahe.shadman@outlook.de>
2026-01-26 17:15:39 +0100

    Remove TODO comments for method definitions
    Removed TODO comments for method definitions in Lasagna class.

--- week4_Java/Lasagna_exercism.java
+++ week4_Java/Lasagna_exercism.java
@@ -1,17 +1,13 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
     public static int expectedMinutesInOven() {
        return 40; 
     }
-    // TODO: define the 'remainingMinutesInOven()' method
     public static int remainingMinutesInOven(int realMinutesInOven) {
         return expectedMinutesInOven() - realMinutesInOven;
         }
-    // TODO: define the 'preparationTimeInMinutes()' method
     public static int preparationTimeInMinutes(int layers) {
         return layers * 2;
     }
-    // TODO: define the 'totalTimeInMinutes()' method
     public static int totalTimeInMinutes(int layers, int realMinutesInOven) {
         return realMinutesInOven + preparationTimeInMinutes(layers);
     }
