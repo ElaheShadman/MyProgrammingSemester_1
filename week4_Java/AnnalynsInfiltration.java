5e65e2b1ea02d2cc418b6bf939c97ee3dbac958d
ElaheShadman <elahe.shadman@outlook.de>
2026-01-26 17:18:44 +0100

    Add canFreePrisoner method to handle rescue logic

--- week4_Java/AnnalynsInfiltration.java
+++ week4_Java/AnnalynsInfiltration.java
@@ -8,3 +8,11 @@ class AnnalynsInfiltration {
 public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
         return !archerIsAwake && prisonerIsAwake;
     }
+   public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+       if (petDogIsPresent) {
+           return !archerIsAwake;
+       } else {
+           return prisonerIsAwake && !knightIsAwake && !archerIsAwake;
+       }
+    }
+}
