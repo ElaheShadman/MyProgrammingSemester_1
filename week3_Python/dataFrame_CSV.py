090e1eb08b7a4ee4a1705979b8653155443e5ee5
ElaheShadman <elahe.shadman@outlook.de>
2026-01-27 14:44:04 +0100

    writing a java code for calculating the mortgage

--- week3_Python/dataFrame_CSV.py
+++ week3_Python/dataFrame_CSV.py
@@ -1,13 +1,10 @@
-# Exercise 2
-
-import pandas as pd
-
-path = "week3_Python\myData.csv"
-df = pd.read_csv(path)
-df.head()
-
-northern_cities = df[df["latitude"] > 0]
-
-average_latitude = northern_cities["latitude"].mean()
-
-print(f"Average Latitude of Northern Cities: {average_latitude:.2f}")
+{
+ "cells": [],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
