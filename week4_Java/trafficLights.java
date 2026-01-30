c6d9fbfc9d375809276e5e848191e9bc3c7d46a6
ElaheShadman <elahe.shadman@outlook.de>
2026-01-30 18:01:41 +0100

    Update trafficLights.java

--- week4_Java/trafficLights.java
+++ week4_Java/trafficLights.java
@@ -2,11 +2,11 @@ public class main {
   public static void main (String[] args) {
     String trafficLights = "red";
     if(trafficlights == "red") {
-      System.out.println("Stop")
+      System.out.println("Stop");
     }else if (trafficLights == "green") {
-      System.out.println("go")
+      System.out.println("go");
     }else {
-      System.out.println("go carefully")
+      System.out.println("go carefully");
     }
   }
 }
