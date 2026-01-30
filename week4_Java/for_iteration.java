ac712b8d3ed24f996f31ecf6147ad93c11caddbf
ElaheShadman <elahe.shadman@outlook.de>
2026-01-30 18:11:49 +0100

    Update for_iteration.java

--- week4_Java/for_iteration.java
+++ week4_Java/for_iteration.java
@@ -1,6 +1,20 @@
+/*
+This whole section is commented out.
+Nothing inside here will run.
+You can put as many lines as you want.
+
 public class main {
   public static void main (String[] args){
     for (int i = 0 ; i < 5 ; i++)
       System.out.println("I learned it..." + i);
   }
+}
+*/
+
+
+public class main {
+  public static void main (String[] args){
+    for (int i = 5 ; i > 0 ; i--)
+      System.out.println("I learned it..." + i);
+ }
 }
