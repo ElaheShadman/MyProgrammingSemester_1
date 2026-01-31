7c6ffe94d9fa9ca7951eb91f60cf82280ef75483
ElaheShadman <elahe.shadman@outlook.de>
2026-01-31 15:56:34 +0100

    Update quersumme.py

--- week3_Python/quersumme.py
+++ week3_Python/quersumme.py
@@ -1,1 +1,7 @@
 # Write a function quersumme(n) that calculates the cross sum of a natural number.
+def quersumme(n):
+    total = 0
+    while n > 0:
+        total += n % 10
+        n = n // 10
+    return total
