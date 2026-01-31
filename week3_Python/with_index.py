e8270587276b02f464b7951ab7dd99353111dbd9
ElaheShadman <elahe.shadman@outlook.de>
2026-01-31 15:42:02 +0100

    Update with_index.py

--- week3_Python/with_index.py
+++ week3_Python/with_index.py
@@ -1,3 +1,5 @@
+# Write a function with_index(l) that takes a list and returns a dictionary where the keys are the indices of the elements in the list, and the values are the corresponding list elements.
+
 def with_index(l):
   d = {}
   for i in range (len(l)):
