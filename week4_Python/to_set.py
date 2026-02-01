472363e623bec1c7cda14af3c5a69b3f3c932286
ElaheShadman <elahe.shadman@outlook.de>
2026-02-01 19:13:13 +0100

    Create to_set.py

---
+++ week4_Python/to_set.py
@@ -0,0 +1,3 @@
+# Write a function to_set(n) that takes a list n , removes all elements from n that occur more than once, and then returns n . The order of the list should remain unchanged.For example, if the list [a, b, c, a, b] is given, the function should return the list [a, b, c] .
+def to_set(n):
+    r = []
