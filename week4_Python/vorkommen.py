3c27fe7563efe62962457650668ba7f17c5c9b68
ElaheShadman <elahe.shadman@outlook.de>
2026-02-01 19:11:35 +0100

    Create vorkommen.py

---
+++ week4_Python/vorkommen.py
@@ -0,0 +1,4 @@
+# Write a function vorkommen(n) that takes a string and returns a dictionary. The dictionary should store each letter in the string as a key and the number of its occurrences within the string as the value. All letters should be interpreted as lowercase.
+def vorkommen(n):
+    n = n.lower()
+    d = {}
