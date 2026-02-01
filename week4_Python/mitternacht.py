3daf3d94e667b87fdaa21c54d6a3729b1e66e556
ElaheShadman <elahe.shadman@outlook.de>
2026-02-01 19:15:09 +0100

    Update mitternacht.py

--- week4_Python/mitternacht.py
+++ week4_Python/mitternacht.py
@@ -1,3 +1,6 @@
 # Write a function mitternacht(a, b, c) that takes three integers (representing the coefficients of a quadratic formula) and returns the sum of all real solutions, converted to an integer. If there is no solution, return 0. It is given that a != 0 .
 def mitternacht(a, b, c):
     d = b*b - 4*a*c
+    if d < 0:
+        return 0
+    return int(-b / a)
