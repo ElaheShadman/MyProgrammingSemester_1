f56413524c99f7e5a4eda11ca08295fe4c808391
ElaheShadman <elahe.shadman@outlook.de>
2026-02-03 20:28:54 +0100

    Data cleaning

--- week4_Python/penguins.py
+++ week4_Python/penguins.py
@@ -9,3 +9,13 @@ print("\nMissing values:")
 print(df.isnull().sum())           # count missing values
 print("\nSummary statistics:")
 print(df.describe())               # numeric summary
+
+
+# Drop rows missing important numeric values
+df_clean = df.dropna(subset=["bill_length_mm", "flipper_length_mm"])
+
+# Fill missing sex values
+df_clean["sex"] = df_clean["sex"].fillna("Unknown")
+
+# Check again
+print(df_clean.isnull().sum())
