04aab94bf5e086eda508b916bda0041499c4a4b6
ElaheShadman <elahe.shadman@outlook.de>
2026-02-03 20:23:43 +0100

    basic EDA

--- week4_Python/penguins.py
+++ week4_Python/penguins.py
@@ -1,3 +1,11 @@
 import pandas as pd
 df = pd.read_csv('penguins.csv')
 print(df.head())
+
+print("Shape:", df.shape)          # number of rows & columns
+print("\nInfo:")
+print(df.info())                   # data types + missing values
+print("\nMissing values:")
+print(df.isnull().sum())           # count missing values
+print("\nSummary statistics:")
+print(df.describe())               # numeric summary
