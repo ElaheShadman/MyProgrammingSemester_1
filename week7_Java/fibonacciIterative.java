715d3d41f25607825ae63d50ce8466ad91be88d3
Elahe Shadman <elahe.shadman@outlook.de>
2026-02-18 09:52:16 +0100

    Implement iterative Fibonacci function

---
+++ week7_Java/fibonacciIterative.java
@@ -0,0 +1,12 @@
+public static int fibonacciIterative(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+
+    int a = 0, b = 1, sum;
+    for (int i = 2; i <= n; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return b;
+}
