d8c10a00903c6071d0450d5db21f1466596b3fc3
Elahe Shadman <elahe.shadman@outlook.de>
2026-02-18 09:27:30 +0100

    Add circle area calculation in circle_area.java

---
+++ week7_Java/circle_area.java
@@ -0,0 +1,7 @@
+public class Main { public static void main(String[] args) { 
+  double radius = 5; 
+  // you can change this value
+  double area = Math.PI * radius * radius; 
+  System.out.println("The area of the circle is: " + area);
+  }
+}                   
