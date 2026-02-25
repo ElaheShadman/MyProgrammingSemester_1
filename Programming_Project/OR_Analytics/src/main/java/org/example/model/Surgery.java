ec7f8984e824488f9b507958f7831e9b6076ae12
Elahe.shadman <Elahe.elahe.shadman@outlook.de>
2026-02-25 20:49:47 +0100

    creating my java project

---
+++ Programming_Project/OR_Analytics/src/main/java/org/example/model/Surgery.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class Surgery {
+    public String procedure;
+    public String surgeon;
+    public String specialty;
+    public String orRoom;
+    public int duration;
+    public int recovery;
+    public String date;     // e.g. "2025-01-05"
+    public int startHour;   // 0–23
+}
