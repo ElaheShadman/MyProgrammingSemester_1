fad70a4b8b98e71e9bfcb56058227bf8d3d95acb
Elahe.shadman <elahe.shadman@outlook.de>
2026-05-26 18:58:05 +0200

    Correct class name and variable casing in trafficLights
    Fix class name and variable casing issues in traffic light logic.

--- week4_Java/trafficLights.java
+++ week4_Java/trafficLights.java
@@ -1,10 +1,12 @@
-public class main {
+sepublic class main {
   public static void main (String[] args) {
     String trafficLights = "red";
     if(trafficlights == "red") {
       System.out.println("Stop")
     } else if (trafficLights == "green") {
       System.out.println("go")
+    } else {
+      System.out.println("go carefully")
     }
   }
 }
