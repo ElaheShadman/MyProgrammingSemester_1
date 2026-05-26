d779c092f19e2fa8d9ec1962f1ef3a55c3e3c845
Elahe.shadman <elahe.shadman@outlook.de>
2026-05-26 18:57:52 +0200

    Add instance creation and print statements for Elahe

--- week3_Python/myFirstObject.py
+++ week3_Python/myFirstObject.py
@@ -2,3 +2,7 @@ class Elahe:
     def __init__(self, age, lastName):
         self.age = age
         self.lastName = lastName
+
+person = Elahe(33, "Shadman")
+print(person.age)        
+print(person.lastName)  
