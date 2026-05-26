39824ed571863b8dbec7122eb64af7fb818b7197
Elahe.shadman <elahe.shadman@outlook.de>
2026-05-26 19:22:45 +0200

    Adding an interest rate

--- Semester_2/Bank/Bank/src/main/java/bank/accounts/SavingsAccount.java
+++ Semester_2/Bank/Bank/src/main/java/bank/accounts/SavingsAccount.java
@@ -1,4 +1,8 @@
 package bank.accounts;
 
-public class SavingsAccount {
+
+
+public class SavingsAccount extends  Account implements InterestBearing {
+
+    private static final double INTEREST_RATE = 0.03;
 }
