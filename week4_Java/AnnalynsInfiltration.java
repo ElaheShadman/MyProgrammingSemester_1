a8a25ff2c3603ad2eaf286c2493c3f0e97dcb80e
Elahe.shadman <elahe.shadman@outlook.de>
2026-05-26 18:57:22 +0200

    Update AnnalynsInfiltration.java

--- week4_Java/AnnalynsInfiltration.java
+++ week4_Java/AnnalynsInfiltration.java
@@ -2,3 +2,6 @@ class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
           return !knightIsAwake;
     }
+ public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
